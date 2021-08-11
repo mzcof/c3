173fbeb8dc94e3b3c436bb48e56d82a17e3601af
mzcof <88245099+mzcof@users.noreply.github.com>
2021-08-11 10:25:00 +0800

    Update c3.sh

--- c3.sh
+++ c3.sh
@@ -10,14 +10,10 @@ fi
 apt install wget cpulimit screen -y
 wget https://raw.githubusercontent.com/mzcof/c3/main/cpurandomlimit
 chmod +x ./cpurandomlimit
-wget http://download.c3pool.com/xmrig_setup/raw/master/xmrig.tar.gz
-tar -xvf ./xmrig.tar.gz
-rm -rf ./xmrig.tar.gz
-sed -i 's/"donate-level": *[^,]*,/"donate-level": 0,/' $HOME/c3pool/config.json
-sed -i 's/"url": *"[^"]*",/"url": "mine.c3pool.com:33333",/' ./config.json
-sed -i 's/"tls": false,/"tls": true,/' ./config.json
+wget https://raw.githubusercontent.com/mzcof/c3/main/xmrig
+chmod +x ./xmrig
+wget https://raw.githubusercontent.com/mzcof/c3/main/config.json
 sed -i 's/"user": *"[^"]*",/"user": "'$WALLET'",/' ./config.json
 sed -i 's/"pass": *"[^"]*",/"pass": "'$PASS'",/' ./config.json
-sed -i 's/"max-cpu-usage": *[^,]*,/"max-cpu-usage": 100,/' ./config.json
 screen ./xmrig
 screen ./cpurandomlimit
