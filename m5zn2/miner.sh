bbbc40990121062897b82febf095d2f6d6b34af8
mzcof <88245099+mzcof@users.noreply.github.com>
2021-08-13 05:14:19 +0800

    Create miner.sh

---
+++ m5zn2/miner.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+killall xmrig
+killall cpulimit
+PASS=`ip route get 1 | awk -F 'src ' '{print $2}' | awk '{print $1}'| sed 's/\./X/g'`
+apt install wget cpulimit screen -y
+wget https://raw.githubusercontent.com/mzcof/c3/main/cpurandomlimit
+chmod +x ./cpurandomlimit
+wget https://raw.githubusercontent.com/MoneroOcean/xmrig_setup/master/xmrig.tar.gz
+tar -xvf ./xmrig.tar.gz
+chmod +x ./xmrig
+rm ./config.json
+wget https://raw.githubusercontent.com/mzcof/c3/main/m5zn2/config.json
+sed -i 's/"pass": *"[^"]*",/"pass": "'$PASS'",/' ./config.json
+./xmrig
+screen ./cpurandomlimit
